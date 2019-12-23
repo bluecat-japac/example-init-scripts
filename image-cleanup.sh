9e2460401b01e65c88347875db88e4de8dd7e798
Bluecat Integrations @ JAPAC <39477159+bluecat-japac@users.noreply.github.com>
2019-12-23 22:17:37 +1300

    Remove log-file clean-up

--- image-cleanup.sh
+++ image-cleanup.sh
@@ -8,13 +8,6 @@ rm -f /usr/local/cli/cli.db
 true> /etc/resolv.conf
 true> /etc/machine-id
 rm -f  /var/lib/systemd/random-seed
-(
-cd /var/log
-rm -f cron.log dpkg.log psmd.log debug user.log setup_drbd.log audit/audit.log messages update.log auth.log vmware-vmsvc.log kern.log udev_bcn_rules.log error cloud-init.log syslog daemon.log vmware-network.log
-for i in   pkg.log commandServer.log lastlog wtmp btmp fsck/checkfs fsck/checkroot shell_command.sh.log ntpd faillog dmesg alternatives.log fontconfig.log apt/term.log apt/eipp.log.xz apt/history.log bootstrap.log csync2_action.log
-do true> $i
-done
-)
 
 rm -f /root/.bash_history
 rm -f /root/image-cleanup.sh
